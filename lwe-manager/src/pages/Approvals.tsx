f430c8ca24b47c4a8f7a8bda7923cdd11a99015a
Sakibur Rahman Oyon <shakiburr312@gmail.com>
2026-07-11 00:49:46 +0600

    Update Approvals.tsx

--- lwe-manager/src/pages/Approvals.tsx
+++ lwe-manager/src/pages/Approvals.tsx
@@ -1,3 +1,4 @@
+//updated
 import React, { useState, useEffect } from 'react';
 import { useAuth } from '../contexts/AuthContext';
 import { watchPendingUsers, approveUser, rejectUser } from '../lib/pendingUsers';
