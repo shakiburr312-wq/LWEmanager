94929b4ee1fbd72c40c4d8bca497a537ba5cf4ac
Sakibur Rahman Oyon <shakiburr312@gmail.com>
2026-07-11 00:55:07 +0600

    Update Finance.tsx

--- lwe-manager/src/pages/Finance.tsx
+++ lwe-manager/src/pages/Finance.tsx
@@ -1,4 +1,3 @@
-//up
 import React, { useState, useEffect } from 'react';
 import { useAuth } from '../contexts/AuthContext';
 import { addFinanceTransaction, watchFinanceTransactions, deleteFinanceTransaction } from '../lib/finance';
