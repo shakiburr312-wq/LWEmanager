38b0beddd1a1511723f584809d46611cad33208e
Sakibur Rahman Oyon <shakiburr312@gmail.com>
2026-07-11 00:50:12 +0600

    Update Complaints.tsx file header comment

--- lwe-manager/src/pages/Complaints.tsx
+++ lwe-manager/src/pages/Complaints.tsx
@@ -1,3 +1,4 @@
+//updated
 import React, { useState, useEffect } from 'react';
 import { useAuth } from '../contexts/AuthContext';
 import { addComplaint, replyToComplaint, watchComplaints, watchPlayerComplaints } from '../lib/complaints';
